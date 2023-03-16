973ad20fb208699583ab4a51374c1f9137234bf7
Daniel Hernandez <dant.hdez02@gmail.com>
2023-03-16 16:59:07 -0600

    Currency Conversor finished

---
+++ index.js
@@ -0,0 +1,33 @@
+const colonesInput = document.getElementById('colon-input');
+const dollarsInput = document.getElementById('dollar-input');
+const tipoCambio = 542.87;
+
+
+
+dollarsInput.addEventListener('input',() => {
+    parseInt(dollarsInput.value);
+    if(isNaN(dollarsInput.value) == true) {
+        alert("Ooops! You have to use a number to do the convertion");
+        dollarsInput.value = "";
+    }
+    else if(dollarsInput.value == ""){
+        colonesInput.value = "";
+    }
+    else {
+        return colonesInput.value = (tipoCambio * dollarsInput.value).toFixed(2);
+    }
+})
+
+colonesInput.addEventListener('input', ()=> {
+    parseInt(colonesInput.value);
+    if(isNaN(colonesInput.value) == true) {
+        alert("Ooops! You have to use a number to do the convertion");
+        colonesInput.value = "";
+    }
+    else if(colonesInput.value == ""){
+        dollarsInput.value = "";
+    }
+    else {
+        return dollarsInput.value = (colonesInput.value / tipoCambio  ).toFixed(2);
+    }
+})
